a02541ed96a85336a5eb253511e93dc075d1db5c
lfmars <lfmars@gmail.com>
2016-05-04 14:40:49 +0300

    added some changes

--- chars.rb
+++ chars.rb
@@ -1,10 +1,21 @@
-file = File.open('info.txt', 'r').read
-sk=0
-hsh=Hash.new(0)
-  file.gsub(" ","").split("").each do |char|
-     hsh[char] +=1
-     sk+=1
+#class Statistic
+ #attr_accessor :file, :hsh
 
-  end
-  puts hsh.sort
-  puts sk
+def readFile
+@file = File.open('text.txt', 'r').read
+end
+
+def calculate
+sk=0
+  @hsh=Hash.new(0)
+   @file.gsub!(/[^a-zA-Z]/, '').split("").each do |char|
+      @hsh[char] +=1
+        sk+=1
+    end
+puts sk
+   end
+
+readFile
+calculate
+puts @hsh.sort
+puts @file
